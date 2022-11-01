35dd8fe2522efa7b1a85abde4d498692fac9a39a
SureshSamarawarna <suresh.asan8@gmail.com>
2022-11-01 16:27:07 +0530

    create doPost method

--- src/main/java/lk/ijse/dep9/api/ItemServlet.java
+++ src/main/java/lk/ijse/dep9/api/ItemServlet.java
@@ -24,7 +24,7 @@ public class ItemServlet extends HTTPServlet2 {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        response.getWriter().println("doPost");
     }
 
     @Override
